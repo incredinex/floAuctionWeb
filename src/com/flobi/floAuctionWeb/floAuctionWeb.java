970f6037e405f29cc7c2ea3f0aea77357710dd9d
flobi <flobi@floci.com>
2014-01-13 15:12:39 -0500

    Basic Bukkit plugin setup

---
+++ src/com/flobi/floAuctionWeb/floAuctionWeb.java
@@ -0,0 +1,23 @@
+package com.flobi.floAuctionWeb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class floAuctionWeb extends JavaPlugin {
+    /**
+     * Called by Bukkit when initializing.  Sets up basic plugin settings.
+     */
+	public void onEnable() {
+	}
+
+	/**
+	 * Loads config.yml and language.yml configuration files.
+	 */
+    private static void loadConfig() {
+    }
+    
+    /**
+     * Called by Bukkit when disabling.  Cancels all auctions and clears data.
+     */
+	public void onDisable() {
+	}
+}
